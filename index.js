c5bc3c236e2bec43a97c1800b08155a8b05be3fa
Stephen Clem <stevebclem@gmail.com>
2015-02-10 17:14:57 -0800

    added some files

---
+++ index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+ $(window).scroll (function () {
+    var sT = $(this).scrollTop();
+    console.log(sT)
+        if (sT >= 572 && sT <= 1089 ) {
+            // $('.navbar').css('backgroundColor', '#e8b71a')
+            $('.navbar a').css('color', '#f7eac8')
+        }else {
+            $('.navbar').css('backgroundColor', '#28abe3')
+            $('.navbar a').css('color', '#f7eac8')
+        }
+  })
+
+})
